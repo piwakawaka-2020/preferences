8584355ba56b04fd7530bedb2408b9b8fc0ed0a2
Richard Kwon <rkminjae@gmail.com>
2020-03-27 12:26:29 +1300

    Add read/write of data file for edit

--- routes/edit.js
+++ routes/edit.js
@@ -9,18 +9,42 @@ router.get('/:name', (req, res) => {
         if(err) throw err
         const users = JSON.parse(data)
         const userName = req.params.name
-        console.log(users)
-        users.people.find(user => user.name === userName)
+        const currentUser = users.people.find(user => user.name === userName)
         
         const viewData = {
-            name: userName,
-            drink: users.drink,
-            sugars: users.sugars,
-            milk: users.milk
+            name: currentUser.name,
+            drink: currentUser.drink,
+            sugars: currentUser.sugars,
+            milk: currentUser.milk
         }
     
         res.render(view, viewData)
     })
+})
+
+router.post('/:name', (req, res) => {
+    fs.readFile('./data.json', 'utf8', (err, data) => {
+        if (err) console.log('An error has occurred')
+
+        const users = JSON.parse(data)
+        users.people.forEach((user, index) => {
+            if(req.params.name === user.name) {
+                console.log(index)
+                users.people[index] = {
+                    name: req.body.name,
+                    drink: req.body.drink,
+                    sugars: Number(req.body.sugars),
+                    milk: Number(req.body.milk)
+                }
+            }
+        })
+        
+        const userJSON = JSON.stringify(users, null, 2)
+        fs.writeFile('./data.json', userJSON, (err, _) => {
+            if (err) console.log('An error has occurred')
+        })
+    })
+    res.redirect(`/edit/${req.body.name}`)
 })
 
 
