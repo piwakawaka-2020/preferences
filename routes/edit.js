2277aa4efa36771ebcf2a779a6d07eef90eeea31
Richard Kwon <rkminjae@gmail.com>
2020-03-27 11:09:56 +1300

    Setup edit router

--- routes/edit.js
+++ routes/edit.js
@@ -1,8 +1,25 @@
 const express = require('express')
 const router = express.Router()
+const fs = require('fs')
 
-router.get('/', (req, res) => {
-    res.render('partials/edit.hbs')
+router.get('/:name', (req, res) => {
+    const view = 'partials/edit'
+
+    fs.readFile('./data.json', 'utf8', (err, data) => {
+        if(err) throw err
+        const users = JSON.parse(data)
+        const userName = req.params.name
+        users.find(user => user.name === userName)
+        
+        const viewData = {
+            name: userName,
+            drink: users.drink,
+            sugars: users.sugars,
+            milk: users.milk
+        }
+    
+        res.render(view, viewData)
+    })
 })
 
 
